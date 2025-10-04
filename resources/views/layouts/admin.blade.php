4dd23ea803421808d0b47e7941c60f3c9bb2382e
SHAHIZZUL IKHWAN BIN SAZALI <shahizzulikhwan@gmail.com>
2025-10-05 01:35:34 +0800

    admin sidebar logo path fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Bookty Enterprise') }} - Admin Dashboard</title>
-    <link rel="icon" type="image/png" href="{{ asset('storage/BooktyLogo/BooktyL.png') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/BooktyL.png') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -44,7 +44,7 @@ aria-label="Sidebar">
   </div>
   <!-- Logo -->
   <div class="flex items-center justify-center mt-6">
-                        <img src="{{ asset('storage/BooktyLogo/BooktyL.png') }}" alt="Bookty Logo" class="h-10 w-auto">
+                        <img src="{{ asset('images/BooktyL.png') }}" alt="Bookty Logo" class="h-10 w-auto">
     <span class="ml-2 text-xl font-serif font-bold text-bookty-purple-700">Bookty Enterprise</span>
                 </div>
 
